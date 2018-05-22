1fac8a4dc86c5f96ce65647ec3924cbf4accf51d
Hannah Mundt <mundth19@hanover.edu>
2018-05-22 16:11:45 -0400

    added title and listview; still need to add functionality

--- app/src/main/java/edu/hanover/cs323_stephaniehannahproject/DeviceActivity.java
+++ app/src/main/java/edu/hanover/cs323_stephaniehannahproject/DeviceActivity.java
@@ -2,6 +2,11 @@ package edu.hanover.cs323_stephaniehannahproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+
 
 public class DeviceActivity extends Activity {
 
@@ -10,4 +15,20 @@ public class DeviceActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device);
     }
+    AdapterView.OnItemClickListener itemClickListener =
+            new AdapterView.OnItemClickListener(){
+                public void onItemClick(AdapterView<?> listView,
+                                        View v,
+                                        int position,
+                                        long id) {
+                    if (position == 0) {
+                        Intent intent = new Intent(DeviceActivity.this,
+                                WelcomeActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            };
+    //Add the listener to the list view
+//    ListView listView = (ListView) findViewById(R.id.list_options);
+//    listView.setOnItemClickListener(itemClickListener);
 }
