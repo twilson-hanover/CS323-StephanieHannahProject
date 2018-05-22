bf0670760dede3ffce201d74f887f00c829f5740
Hannah Mundt <mundth19@hanover.edu>
2018-05-22 15:01:36 -0400

    added button; still have to add it's functionality

--- app/src/main/java/edu/hanover/cs323_stephaniehannahproject/WelcomeActivity.java
+++ app/src/main/java/edu/hanover/cs323_stephaniehannahproject/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package edu.hanover.cs323_stephaniehannahproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+    public void onClickNextWelcome(View view){
+        startActivity(new Intent(WelcomeActivity.this, DeviceActivity.class));
+        //click button to go to next page
     }
 }
