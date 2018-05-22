157d0468302ffb6c68541238e535afcc1d2e6ed5
Hannah Mundt <mundth19@hanover.edu>
2018-05-22 14:46:58 -0400

    edit activity_welcome

--- app/src/main/java/edu/hanover/cs323_stephaniehannahproject/WelcomeActivity.java
+++ app/src/main/java/edu/hanover/cs323_stephaniehannahproject/WelcomeActivity.java
@@ -1,14 +1,13 @@
 package edu.hanover.cs323_stephaniehannahproject;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
-
 }
